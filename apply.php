9782276553ec9f69a778f7ba776910f5aa6cccae
Rachael97 <kaijing0319@gmail.com>
2017-05-05 09:17:30 +0800

    apply.php
    1.javascript预设购买配套
    2.type改name (php 需要name)
    3.php POST接收资料

---
+++ apply.php
@@ -0,0 +1,61 @@
+<?php
+	
+	$companyName = $_POST['companyName'];
+	echo $companyName;
+	
+	$companyCode = $_POST['companyCode'];
+	echo $companyCode;
+	
+	$companyEmail = $_POST['companyEmail'];
+	echo $companyEmail;
+
+	$companyTel = $_POST['companyTel'];
+	echo $companyTel;
+	
+	$package = $_POST['package'];
+	echo $package;
+	
+	$country = $_POST['country'];
+	echo $country;
+	
+	$companySsm = $_POST['companySsm'];
+	echo $companySsm;
+	
+	$companyGst = $_POST['companyGst'];
+	echo $companyGst;
+	
+	$coverArea = $_POST['coverArea'];
+	echo $coverArea;
+	
+	$targetCustomer = $_POST['targetCustomer'];
+	echo $targetCustomer;
+	
+	$targetPeriod = $_POST['targetPeriod'];
+	echo $targetPeriod;
+	
+	$customerName = $_POST['customerName'];
+	echo $customerName;
+	
+	$firstName = $_POST['firstName'];
+	echo $firstName;
+	
+	$lastName = $_POST['lastName'];
+	echo $lastName;
+	
+	$loginId = $_POST['loginId'];
+	echo $loginId;
+	
+	$customerEmail = $_POST['customerEmail'];
+	echo $customerEmail;
+	
+	$customerTel = $_POST['customerTel'];
+	echo $customerTel;
+	
+	$password = $_POST['password'];
+	echo $password;
+	
+	$supplementaryDocument = $_POST['supplementaryDocument'];
+	echo $supplementaryDocument;
+	
+	
+?>
